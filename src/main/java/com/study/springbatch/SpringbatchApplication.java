c6d6898dda5755b2c7455711a8f73f01b60cf3df
jiwon615 <seg615@naver.com>
2023-03-10 19:55:13 +0900

    feat: JobExecution FAILE과 COMPLETE 이해 예제 V3

--- src/main/java/com/study/springbatch/SpringbatchApplication.java
+++ src/main/java/com/study/springbatch/SpringbatchApplication.java
@@ -2,6 +2,8 @@ package com.study.springbatch;
 
 import com.study.springbatch.config.v2.JobParametersConfigV2;
 import com.study.springbatch.config.v2.JobRunnerV2;
+import com.study.springbatch.config.v3.JobExecutionConfigV3;
+import com.study.springbatch.config.v3.JobRunnerV3;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +11,8 @@ import org.springframework.context.annotation.Import;
 
 //@Import(HelloJobConfiguration.class)
 //@Import({JobInstanceConfigV1.class, JobRunnerV1.class})
-@Import({JobParametersConfigV2.class, JobRunnerV2.class})
+//@Import({JobParametersConfigV2.class, JobRunnerV2.class})
+@Import({JobExecutionConfigV3.class, JobRunnerV3.class})
 @EnableBatchProcessing
 @SpringBootApplication(scanBasePackages = "com.study.springbatch.basic")
 public class SpringbatchApplication {
