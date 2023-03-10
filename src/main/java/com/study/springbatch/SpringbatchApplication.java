e959c6e5bc5828945af5dbea0604f8584bd90962
jiwon615 <seg615@naver.com>
2023-03-10 18:19:35 +0900

    feat: jobInstance와 jobParameter 이해하기 v1

--- src/main/java/com/study/springbatch/SpringbatchApplication.java
+++ src/main/java/com/study/springbatch/SpringbatchApplication.java
@@ -1,11 +1,16 @@
 package com.study.springbatch;
 
+import com.study.springbatch.config.v1.JobConfigurationV1;
+import com.study.springbatch.config.v1.JobRunnerV1;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+//@Import(HelloJobConfiguration.class)
+@Import({JobConfigurationV1.class, JobRunnerV1.class})
 @EnableBatchProcessing
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.study.springbatch.basic")
 public class SpringbatchApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/study/springbatch/config/HelloJobConfiguration.java
+++ src/main/java/com/study/springbatch/config/HelloJobConfiguration.java
@@ -1,4 +1,4 @@
-package com.study.springbatch;
+package com.study.springbatch.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
