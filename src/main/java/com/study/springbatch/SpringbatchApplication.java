c0c7d105612bf1cd5c5c4a5a228f3ed468ea04b9
jiwon615 <seg615@naver.com>
2023-03-10 19:03:54 +0900

    modify: import 빠진 부분 추가

--- src/main/java/com/study/springbatch/SpringbatchApplication.java
+++ src/main/java/com/study/springbatch/SpringbatchApplication.java
@@ -1,14 +1,15 @@
 package com.study.springbatch;
 
 import com.study.springbatch.config.v2.JobParametersConfigV2;
+import com.study.springbatch.config.v2.JobRunnerV2;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 //@Import(HelloJobConfiguration.class)
-//@Import({JobInstanceConfigV1.class, JobInstanceRunnerV1.class})
-@Import({JobParametersConfigV2.class})
+//@Import({JobInstanceConfigV1.class, JobRunnerV1.class})
+@Import({JobParametersConfigV2.class, JobRunnerV2.class})
 @EnableBatchProcessing
 @SpringBootApplication(scanBasePackages = "com.study.springbatch.basic")
 public class SpringbatchApplication {
