1a12345ad4db3adc5a7da1de700f325dc5a91da1
jiwon615 <seg615@naver.com>
2023-03-11 12:43:19 +0900

    feat: JobListener 등록 및 custom JobRepository활용 V6

--- src/main/java/com/study/springbatch/SpringbatchApplication.java
+++ src/main/java/com/study/springbatch/SpringbatchApplication.java
@@ -3,6 +3,10 @@ package com.study.springbatch;
 import com.study.springbatch.config.v4.JobRunnerV4;
 import com.study.springbatch.config.v4.StepExecutionConfigV4;
 import com.study.springbatch.config.v5.*;
+import com.study.springbatch.config.v6.CustomBatchConfigurer;
+import com.study.springbatch.config.v6.JobRepositoryConfigV6;
+import com.study.springbatch.config.v6.JobRepositoryListener;
+import com.study.springbatch.config.v6.JobRunnerV6;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,7 +17,8 @@ import org.springframework.context.annotation.Import;
 //@Import({JobParametersConfigV2.class, JobRunnerV2.class})
 //@Import({JobExecutionConfigV3.class, JobRunnerV3.class})
 //@Import({StepExecutionConfigV4.class, JobRunnerV4.class})
-@Import({ExecutionContextConfigV5.class, JobRunnerV5.class, ExecutionTasklet1.class, ExecutionTasklet2.class, ExecutionTasklet3.class, ExecutionTasklet4.class})
+//@Import({ExecutionContextConfigV5.class, JobRunnerV5.class, ExecutionTasklet1.class, ExecutionTasklet2.class, ExecutionTasklet3.class, ExecutionTasklet4.class})
+@Import({JobRepositoryConfigV6.class, JobRunnerV6.class, JobRepositoryListener.class, CustomBatchConfigurer.class})
 @EnableBatchProcessing
 @SpringBootApplication(scanBasePackages = "com.study.springbatch.basic")
 public class SpringbatchApplication {
---
+++ src/main/java/com/study/springbatch/config/v6/JobRepositoryConfigV6.java
@@ -0,0 +1,58 @@
+package com.study.springbatch.config.v6;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class JobRepositoryConfigV6 {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final JobRepositoryListener jobRepositoryListener;
+
+    @Bean
+    public Job job() {
+        log.info(">> job_v6");
+        return jobBuilderFactory.get("job_v6")
+                .start(step1())
+                .next(step2())
+                .listener(jobRepositoryListener) // 빈으로 만들어준 리스너 등록!
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1_v6")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        log.info(">> step1_v6");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2_v6")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">> step2_v6");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
