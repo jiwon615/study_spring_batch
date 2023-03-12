6b5c4e5203ffaa142679da0c128bdb181bf90e16
jiwon615 <seg615@naver.com>
2023-03-12 14:07:59 +0900

    feat: JobBuilderFacotry 학습하면 Flow 객체 생성 예시 작성- part2_v2

--- src/main/java/com/study/springbatch/SpringbatchApplication.java
+++ src/main/java/com/study/springbatch/SpringbatchApplication.java
@@ -2,6 +2,8 @@ package com.study.springbatch;
 
 import com.study.springbatch.config.part2.v1.FirstJobConfigV1;
 import com.study.springbatch.config.part2.v1.SecondJobConfigV1;
+import com.study.springbatch.config.part2.v2.JobBuilderFactoryConfigV2;
+import com.study.springbatch.config.part2.v2.JobRunnerV2;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,7 +17,8 @@ import org.springframework.context.annotation.Import;
 //@Import({ExecutionContextConfigV5.class, JobRunnerV5.class, ExecutionTasklet1.class, ExecutionTasklet2.class, ExecutionTasklet3.class, ExecutionTasklet4.class})
 //@Import({JobRepositoryConfigV6.class, JobRunnerV6.class, JobRepositoryListener.class, CustomBatchConfigurer.class})
 //@ComponentScan("com.study.springbatch.config.part1.v7")
-@Import({FirstJobConfigV1.class, SecondJobConfigV1.class})
+//@Import({FirstJobConfigV1.class, SecondJobConfigV1.class}) // 여기서부터 part2
+@Import({JobBuilderFactoryConfigV2.class, JobRunnerV2.class})
 @EnableBatchProcessing
 @SpringBootApplication(scanBasePackages = "com.study.springbatch.basic")
 public class SpringbatchApplication {
